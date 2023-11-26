0e2e85d38c23381eb39f4b2c1bcbb2b6dd843b32
rickgurgel <rgurgelf@gmail.com>
2023-11-26 13:06:52 -0300

    first commit -> creating project

---
+++ src/main/java/br/gurgel/palavra/PalavraApplication.java
@@ -0,0 +1,13 @@
+package br.gurgel.palavra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalavraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PalavraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/gurgel/palavra/PalavraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gurgel.palavra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PalavraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
